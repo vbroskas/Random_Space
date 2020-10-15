82e275a22f0da55542beaaf356d779464ad7c5ea
loren <baca.loren@gmail.com>
2020-10-15 14:21:53 -0600

    pushing to prod, working on statics error

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :space, SpaceWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "gigalixirapp.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//spacedout.gigalixirapp.com"],
 
 # Do not print debug messages in production
 config :logger, level: :info
